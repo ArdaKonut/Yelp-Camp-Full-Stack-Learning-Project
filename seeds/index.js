aa6389557d1cd3f79076d62a424fb26201de30a8
ArdaKonut <ardakonut74@gmail.com>
2026-05-08 14:29:40 +0300

    Server Side & Client Side Validation and Error Handling

--- seeds/index.js
+++ seeds/index.js
@@ -1,40 +1,44 @@
 const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
+const cities = require("./cities");
 const Campground = require('../models/campground');
+const { places, descriptors } = require('./seedHelpers');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+.then(() => {
+    console.log("Mongo connection open")
+})
+.catch(err => {
+    console.log("Mongo connection error: ")
+    console.log(err);
 });
 
+// Basic logic to check whether the DB is successfully up and running!
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+    console.log("Database Connected!");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = array => array[Math.floor(Math.random() * array.length)]
 
-
-const seedDB = async () => {
+const seedDb = async() => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for(let i=0; i<50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
+        const price =  Math.floor(Math.random() * 30) + 10;
+        const camp =  new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere que repudiandae, perspiciatis debitis! Optio?',
+            price: price
+
+
         })
         await camp.save();
     }
 }
 
-seedDB().then(() => {
+seedDb().then(() => {
     mongoose.connection.close();
 })
